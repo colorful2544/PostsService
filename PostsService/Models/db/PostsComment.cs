cb97c424b7f5c58c09c0ee93f9b506212fa2e237
Bike Chanokchon <mint.colorful@gmail.com>
2021-09-04 16:03:20 +0700

    connection to sqlserver

---
+++ PostsService/Models/db/PostsComment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace PostsService.Models.db
+{
+    public partial class PostsComment
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string PostId { get; set; }
+        public DateTime Created { get; set; }
+        public string Detail { get; set; }
+
+        public virtual Post Post { get; set; }
+        public virtual User User { get; set; }
+    }
+}
