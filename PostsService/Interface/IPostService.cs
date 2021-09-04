8756fb836ca8a833ae80001b7ec2415b9e2a0305
Bike Chanokchon <mint.colorful@gmail.com>
2021-09-04 17:21:38 +0700

    create ViewModel PostRequest

---
+++ PostsService/Interface/IPostService.cs
@@ -0,0 +1,13 @@
+﻿using PostsService.Models.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostsService.Interface
+{
+    public interface IPostService
+    {
+
+    }
+}
---
+++ PostsService/ViewModels/PostServiceViewModel/PostRequest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostsService.ViewModels.PostServiceViewModel
+{
+    public class PostRequest
+    {
+        public string Name { get; set; }
+        public List<IFormFile> imagesList { get; set; }
+        public string detail {  get; set; }
+    }
+}
